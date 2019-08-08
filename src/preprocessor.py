5a4fdd2e1a52ea9abb372f4d9bd7b6a6e2eeeafe
Izzy <avrisaac555@gmail.com>
2019-08-08 10:03:59 -0700

    Added tokens for <= and >=

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -34,6 +34,8 @@ class TokenType(Enum):
     RPARAN = ")"
     QUOTE = "\""
     COMMA = ","
+    LESS_THAN_OR_EQUAL = "<="
+    GREATER_THAN_OR_EQUAL = ">="
     LESS_THAN = "<"
     GREATER_THAN = ">"
     EQUAL = "=="
