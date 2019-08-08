c59dc37909875593015fdf33cade6d0dd88939a9
Izzy <avrisaac555@gmail.com>
2019-08-07 20:14:50 -0700

    Redoing preprocessor

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -5,125 +5,100 @@ the purpose of this preprocessor is to determine the relationship between compil
 
 import re
 import string
-from enum import Enum, auto
-from typing import Optional
+from enum import Enum, unique
+from typing import Optional, List, NamedTuple, Iterable
 
-LINE_RE = re.compile(r"#(?P<directive>\S*)(?: (?P<expression>.*))?")
-OBJECT_MACRO_RE = re.compile(r"^(?:#define )?(?P<identifier>[^\s\(\)\#]*) (?P<tokens>.*)")
-FUNCTION_MACRO_RE = re.compile(r"^(?:#define )?(?P<identifier>\S*)(?P<args>\(.*\)) (?P<tokens>.*)")
-CONSTANT_COMPARISON_EXPR_RE = re.compile(r"^(?P<p_1>\w+) ?(?P<operator>[=<>!]+) ?(?P<p_2>.*)")
-
-
-class PreProcessorParseError(Exception):
-    """Thrown when the preprocessor parser encounters an error"""
-
-    def __init__(self, line, reason):
-        self.line = line
-        self.reason = reason
+DIRECTIVE_RE = re.compile(r"^#(?P<directive>\S*)(?: (.*))?")
+INT_CONST_RE = re.compile(r"^(\d+)")
+CHAR_CONST_RE = re.compile(r"^('\S')")
+IDENTIFIER_RE = re.compile(r"(\w+)")
 
 
-class MatchError(Exception):
-    """Thrown when a regex match fails (say for an expression). This is intended to be caught."""
-    pass
+@unique
+class TokenType(Enum):
+    DIRECTIVE = 0
+    IDENTIFIER = 1
+    INTEGER_CONST = 2
+    CHAR_CONST = 3
+    LPARAN = "("
+    RPARAN = ")"
+    QUOTE = "\""
+    COMMA = ","
+    LESS_THAN = "<"
+    GREATER_THAN = ">"
+    EQUAL = "=="
+    NOT = "!"
+    AND = "&&"
+    OR = "||"
+    DEFINED = "defined"
+    ELLIPSIS = "..."
+    TOKEN_STRINGIFICATION = "#"
+    TOKEN_CONCATINATION = "##"
 
 
-class UnknownTokenError(Exception):
-    def __init__(self, token):
-        self.token = token
-
-    def __str__(self):
-        return "Unknown token: " + self.token
-
-
-class Directives(Enum):
-    INCLUDE = auto()
-    DEFINE = auto()
-    IF = auto()
-    IFDEF = auto()
-    IFNDEF = auto()
-    ELIF = auto()
-    ELSE = auto()
-    ENDIF = auto()
-    PRAGMA = auto()
+class Token(NamedTuple):
+    token_type: TokenType
+    line: int
+    col: int
+    text: str
 
 
-class ComparisonOperators(Enum):
-    EQUAL = auto()
-    LESS_THAN = auto()
-    GREATER_THAN = auto()
-    LESS_THAN_OR_EQUAL = auto()
-    GREATER_THAN_OR_EQUAL = auto()
-
-    @staticmethod
-    def from_string(cls, operator_string: str) -> "ComparisonOperators":
-        return {
-            "==": cls.EQUAL,
-            "<": cls.LESS_THAN,
-            ">": cls.GREATER_THAN,
-            "<=": cls.LESS_THAN_OR_EQUAL,
-            ">=": cls.GREATER_THAN_OR_EQUAL
-        }[operator_string]
-
-
-class ComparisonExpression():
-    @staticmethod
-    def from_expr(cls, expr: str) -> "ComparisonExpression":
-        expr_match = CONSTANT_COMPARISON_EXPR_RE.match(expr)
-        if not expr_match:
-            raise MatchError()
-
-        try:
-            operator_string = expr_match.group("operator")
-            operator_value = ComparisonOperators.from_string(operator_string)
-        except KeyError:
-            raise UnknownTokenError(operator_string)
-
-        left_parameter = expr_match("p_1")
-        right_parameter = expr_match("p_2")
-
-        return cls(operator_value, left_parameter, right_parameter)
-
-    def __init__(self, op: ComparisonOperators, left_parameter: str, right_parameter: str):
-        self.op = op
-        self.left_parameter = left_parameter
-        self.right_parameter = right_parameter
-
-
-def parse_line(line: str) -> Optional[tuple]:
-    line_match = LINE_RE.match(line)
-
-    if not line_match:
+def _tokenize_line(line: str, line_number: int) -> Optional[List[Token]]:
+    if not line.startswith("#"):
         return None
 
-    try:
-        # Enum determines valid directives
-        directive = Directives[line_match.group("directive").upper()]
-        expr = line_match.group("expression")
-    except KeyError:
-        raise PreProcessorParseError(line, "Invalid preprocessor directive: " + line_match.group("directive"))
+    current_index = line.index(" ", 1)
+    line_tokens = [Token(TokenType.DIRECTIVE, line_number, 1, line[1:current_index])]
 
-    if directive is Directives.DEFINE:
-        # Need to figure out if this is an object macro or a function macro
-        expr_match = OBJECT_MACRO_RE.match(expr)
-        if expr_match is not None:
-            return (directive, expr_match.group("identifier"), expr_match.group("tokens"))
+    while current_index < len(line):
+        if line[current_index].isspace():
+            current_index += 1
+            continue
 
-        expr_match = FUNCTION_MACRO_RE.match(expr)
-        if expr_match is not None:
-            args_list = [x.strip() for x in expr_match.group("args").strip(string.whitespace + "()").split(",")]
-            return (directive, expr_match.group("identifier"), args_list, expr_match.group("tokens"))
+        token = None
 
-        raise PreProcessorParseError(line, "Invalid Macro Definition")
-    elif directive is Directives.IF or directive is Directives.ELIF:
-        # TODO: Need to implement full expression parsing, allowing for logical and arithmetic operators
-        try:
-            comp_expr = ComparisonExpression.from_expr(expr)
-            return (directive, comp_expr)
-        except MatchError:
-            raise PreProcessorParseError(line, "Invalid expression")
-        except UnknownTokenError as e:
-            raise PreProcessorParseError(line, str(e))
-    else:
-        return (directive, expr)
+        # Fixed-length tokens
+        for name, member in TokenType.__members__.items():
+            # Skip over non-string members, they're for regex
+            if type(member.value) is not str:
+                continue
+
+            if line.startswith(member.value, current_index):
+                token = Token(member, line_number, current_index, member.value)
+                break
+
+        # Check if a token has been found, if it has, skip over regex
+        if token:
+            line_tokens.append(token)
+            current_index += len(token.text)
+            continue
+
+        # Run the regex searches for variable-length tokens
+        re_match = INT_CONST_RE.match(line, pos=current_index)
+        if re_match:
+            token = Token(TokenType.INTEGER_CONST, line_number, current_index, re_match.group(0))
+
+        re_match = CHAR_CONST_RE.match(line, pos=current_index)
+        if re_match:
+            token = Token(TokenType.CHAR_CONST, line_number, current_index, re_match.group(0))
+
+        re_match = IDENTIFIER_RE.match(line, pos=current_index)
+        if re_match:
+            token = Token(TokenType.IDENTIFIER, line_number, current_index, re_match.group(0))
+
+        if token:
+            line_tokens.append(token)
+            current_index += len(token.text)
+        else:
+            raise Exception
 
 
+def tokenize_lines(source_lines: Iterable[str]) -> List[List[Token]]:
+    """
+    Takes a source file split into lines and tokeninzes it.
+    C code (lines which do not start with "#") are ignored.
+    """
+    tokenized_lines = []
+
+    for line in source_lines:
+        line_match = 
