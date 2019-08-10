53015f35d90f2f5831f7c4e24ca347b90ea28e35
Izzy <avrisaac555@gmail.com>
2019-08-10 09:20:32 -0700

    Added an exception type for syntax errors in the preprocessor

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -19,6 +19,16 @@ class UnknownTokenError(Exception):
         return f"Unknown token \"{self.token}\" on line {self.line_number}, {self.column_number}"
 
 
+class PreprocessorSyntaxError(Exception):
+    def __init__(self, line_number, column_number, message):
+        self.line_number = line_number
+        self.column_number = column_number
+        self.message = message
+
+    def __str__(self):
+        return f"Syntax error on line {self.line_number}, {self.column_number}: {self.message}"
+
+
 @unique
 class TokenType(Enum):
     DIRECTIVE = auto()
