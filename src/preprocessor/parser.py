fba6498572587be121a239b3f5a56bde9aa77707
Izzy <avrisaac555@gmail.com>
2019-09-22 18:13:01 -0700

    Put expression parsing algorithm into expression class

--- src/preprocessor/parser.py
+++ src/preprocessor/parser.py
@@ -25,8 +25,8 @@ def _expect_token(tokens: List[Token], expected_types: Set[TokenType], pos: int 
     return tokens.pop(pos)
 
 
-def __get_precidence(op: TokenType) -> int:
-    PRECIDENCE_MAP = (
+class Expression:
+    __PRECIDENCE_MAP = (
         ({TokenType.DEFINED, TokenType.NOT}, 100),
         ({TokenType.GREATER_THAN_OR_EQUAL, TokenType.LESS_THAN_OR_EQUAL,
             TokenType.LESS_THAN, TokenType.GREATER_THAN}, 90),
@@ -35,65 +35,48 @@ def __get_precidence(op: TokenType) -> int:
         (set(TokenType.OR), 40)
     )
 
-    for comp_set, value in PRECIDENCE_MAP:
-        if op in comp_set:
-            return value
-    return 0
-
-
-def _compare_operators(op1: TokenType, op2: TokenType):
-    """
-    Returns 1 if op1 has a higher precidence than op2,
-            0 if op1 and op2 have the sace precidence,
-            -1 if op1 has a lower precidence than op2
-    """
-    v = __get_precidence(op1) - __get_precidence(op2)
-
-    if v > 0:
-        return 1
-    elif v < 0:
-        return -1
-    else:
+    @staticmethod
+    def __get_operator_precidence(op: TokenType) -> int:
+        for comp_set, value in Expression.__PRECIDENCE_MAP:
+            if op in comp_set:
+                return value
         return 0
 
+    @staticmethod
+    def _compare_operators(op1: TokenType, op2: TokenType):
+        """
+        Returns 1 if op1 has a higher precidence than op2,
+                0 if op1 and op2 have the sace precidence,
+                -1 if op1 has a lower precidence than op2
+        """
+        v = Expression.__get_precidence(op1) - Expression.__get_precidence(op2)
 
-def _read_conditional_expression(tokens: List[Token]):
-    """
-    Reads a conditional expression from tokens into an RPN stack.
-    `tokens` should be a be a list of that tokens that only make up the conditional expression
-    """
-    output_stack: List[Token] = []
-    operator_stack: List[Token] = []
+        if v > 0:
+            return 1
+        elif v < 0:
+            return -1
+        else:
+            return 0
 
-    while tokens:
-        tok = tokens.pop(0)
+    @staticmethod
+    def _push_operator(op_tok: Token, operator_stack: List[Token], output_stack: List[Token]):
+        while operator_stack:
+            op_peek = operator_stack[-1]
+            op_comp = Expression._compare_operators(op_peek.token_type, op_tok.token_type)
 
-        if tok.token_type in VALUE_TOKENS:
-            output_stack.append(tok)
-            continue
+            if op_peek.token_type is not TokenType.LPARAN and (
+               op_peek.token_type in RTL_OPS or op_comp == 1):
+                output_stack.append(operator_stack.pop())
+            else:
+                break
 
-        # Push right-to-left associative operators to the operator stack
-        # TODO: Add support for function here
-        if tok.token_type in RTL_OPS:
-            operator_stack.append(tok)
-            continue
+        operator_stack.append(op_tok)
 
-        if tok.token_type in (OPERATOR_TOKENS - RTL_OPS):
-            while operator_stack:
-                op_peek = operator_stack[-1]
-                op_comp = _compare_operators(op_peek.token_type, tok.token_type)
-
-                if op_peek.token_type is not TokenType.LPARAN and (op_peek.token_type in RTL_OPS or op_comp == 1):
-                    output_stack.append(operator_stack.pop())
-                else:
-                    break
-
-            operator_stack.append(tok)
-
-        if tok.token_type is TokenType.LPARAN:
-            operator_stack.append(tok)
-
-        if tok.token_type is TokenType.RPARAN:
+    @staticmethod
+    def _push_parenthesis(paran_tok: Token, operator_stack: List[Token], output_stack: List[Token]):
+        if paran_tok.token_type is TokenType.LPARAN:
+            operator_stack.append(paran_tok)
+        elif paran_tok.token_type is TokenType.RPARAN:
             try:
                 while operator_stack[-1].token_type is not TokenType.LPARAN:
                     output_stack.append(operator_stack.pop())
@@ -102,12 +85,43 @@ def _read_conditional_expression(tokens: List[Token]):
                 operator_stack.pop()
             except IndexError:
                 # Stack ran out without finding an LPARAN, we have mismatched parenthesis
-                raise PreprocessorSyntaxError(tok.line, tok.col, "Unexpected ')'.")
-    
-    # Push all remaining operators on output stack. Note that list.extends is not used to check for mismatched parenthesis
-    for operator in reversed(operator_stack):
-        if operator.token_type is in {TokenType.LPARAN, TokenType.RPARAN}:
-            raise PreprocessorSyntaxError(operator.line, operator.col, "Unexpected paranthesis")
-        output_stack.append(operator)
-    
-    return output_stack
+                raise PreprocessorSyntaxError(paran_tok.line, paran_tok.col, "Unexpected ')'.")
+
+    @staticmethod
+    def push_operator_stack(operator_stack: List[Token], output_stack: List[Token]):
+        for operator in reversed(operator_stack):
+            if operator.token_type in {TokenType.LPARAN, TokenType.RPARAN}:
+                raise PreprocessorSyntaxError(operator.line, operator.col, "Unexpected paranthesis")
+            output_stack.append(operator)
+
+    @classmethod
+    def from_tokens(cls, tokens: List[Token]):
+        """
+        Reads a conditional expression from tokens into an RPN stack.
+        `tokens` should be a be a list of that tokens that only make up the conditional expression
+        """
+        output_stack: List[Token] = []
+        operator_stack: List[Token] = []
+
+        while tokens:
+            tok = tokens.pop(0)
+
+            if tok.token_type in VALUE_TOKENS:
+                output_stack.append(tok)
+
+            # Push right-to-left associative operators to the operator stack
+            # TODO: Add support for function here
+            elif tok.token_type in RTL_OPS:
+                operator_stack.append(tok)
+            elif tok.token_type in (OPERATOR_TOKENS - RTL_OPS):
+                cls._push_operator(tok, operator_stack, output_stack)
+            elif tok.token_type in {TokenType.LPARAN, TokenType.RPARAN}:
+                cls._push_parenthesis(tok, operator_stack, output_stack)
+
+        # Push all remaining operators on output stack. Note that list.extends is not used to check for mismatched parenthesis
+        cls._push_operator_stack(operator_stack, output_stack)
+
+        return cls(output_stack)
+
+    def __init__(self, expression_stack: List[Token]):
+        self.expression_stack = expression_stack
