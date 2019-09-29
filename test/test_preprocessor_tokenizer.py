3c70818d27f129fa89dbc29a48b7ddfb8b8b5cbb
Izzy <avrisaac555@gmail.com>
2019-09-29 16:24:30 -0700

    Tokenizer tests use dicts instead of tuples

--- test/test_preprocessor_tokenizer.py
+++ test/test_preprocessor_tokenizer.py
@@ -3,19 +3,25 @@ from src.preprocessor.tokenizer import tokenize_line, Token, TokenType, _tokeniz
 from src.useful import StringCursor
 
 
-def assert_token_equals(token: Token, type: TokenType, line: int, col: int, matched: str):
+def assert_token_equals(actual: Token, type: TokenType, line=None, col=None, matched=None):
     """Helper function to test token equality"""
-    assert token.token_type is type
-    assert token.line == line
-    assert token.col == col
-    assert token.match.group() == matched
+    assert actual.token_type is type
+
+    if line is not None:
+        assert actual.line == line
+
+    if col is not None:
+        assert actual.col == col
+
+    if matched is not None:
+        assert actual.match.group() == matched
 
 
-def assert_token_lists_equal(actual: list, expected: list):
+def assert_token_lists_equal(actual: list, expected: dict):
     assert len(actual) == len(expected)
 
     for a, e in zip(actual, expected):
-        assert_token_equals(a, *e)
+        assert_token_equals(a, **e)
 
 
 def test_tokenize_directive_valid_input():
@@ -41,52 +47,231 @@ def test_tokenize_directive_error():
 
 
 test_data = [
-    ("Null directive", "#\n", 0, [(TokenType.DIRECTIVE, 0, 1, "#")]),
-    ("No parameter directive", "#pragma\n", 0, [(TokenType.DIRECTIVE, 0, 1, "#pragma")]),
+    ("Null directive", "#\n", 0, [
+        {
+            "type": TokenType.DIRECTIVE,
+            "line": 0,
+            "col": 1,
+            "matched": "#"
+        }
+    ]),
+    ("No parameter directive", "#pragma\n", 0, [
+        {
+            "type": TokenType.DIRECTIVE,
+            "line": 0,
+            "col": 1,
+            "matched": "#pragma"
+        }
+    ]),
     ("Integer constant", "#define TEST_INT 1234\n", 4, [
-        (TokenType.DIRECTIVE, 4, 1, "#define"),
-        (TokenType.IDENTIFIER, 4, 8, "TEST_INT"),
-        (TokenType.INTEGER_CONST, 4, 17, "1234")
+        {
+            "type": TokenType.DIRECTIVE,
+            "line": 4,
+            "col": 1,
+            "matched": "#define"
+        },
+        {
+            "type": TokenType.IDENTIFIER,
+            "line": 4,
+            "col": 8,
+            "matched": "TEST_INT"
+        },
+        {
+            "type": TokenType.INTEGER_CONST,
+            "line": 4,
+            "col": 17,
+            "matched": "1234"
+        }
     ]),
     ("Character constant", "#define TEST_CHAR 's'\n", 5, [
-        (TokenType.DIRECTIVE, 5, 1, "#define"),
-        (TokenType.IDENTIFIER, 5, 8, "TEST_CHAR"),
-        (TokenType.CHAR_CONST, 5, 18, "'s'")
+        {
+            "type": TokenType.DIRECTIVE,
+            "line": 5,
+            "col": 1,
+            "matched": "#define"
+        },
+        {
+            "type": TokenType.IDENTIFIER,
+            "line": 5,
+            "col": 8,
+            "matched": "TEST_CHAR"
+        },
+        {
+            "type": TokenType.CHAR_CONST,
+            "line": 5,
+            "col": 18,
+            "matched": "'s'"
+        }
     ]),
     ("Function macro", "#define FMACRO(name) typedef struct name##_s name##_t\n", 6, [
-        (TokenType.DIRECTIVE, 6, 1, "#define"),
-        (TokenType.IDENTIFIER, 6, 8, "FMACRO"),
-        (TokenType.LPARAN, 6, 14, "("),
-        (TokenType.IDENTIFIER, 6, 15, "name"),
-        (TokenType.RPARAN, 6, 19, ")"),
-        (TokenType.IDENTIFIER, 6, 21, "typedef"),
-        (TokenType.IDENTIFIER, 6, 29, "struct"),
-        (TokenType.IDENTIFIER, 6, 36, "name"),
-        (TokenType.TOKEN_CONCATINATION, 6, 40, "##"),
-        (TokenType.IDENTIFIER, 6, 42, "_s"),
-        (TokenType.IDENTIFIER, 6, 45, "name"),
-        (TokenType.TOKEN_CONCATINATION, 6, 49, "##"),
-        (TokenType.IDENTIFIER, 6, 51, "_t")
+        {
+            "type": TokenType.DIRECTIVE,
+            "line": 6,
+            "col": 1,
+            "matched": "#define"
+        },
+        {
+            "type": TokenType.IDENTIFIER,
+            "line": 6,
+            "col": 8,
+            "matched": "FMACRO"
+        },
+        {
+            "type": TokenType.LPARAN,
+            "line": 6,
+            "col": 14,
+            "matched": "("
+        },
+        {
+            "type": TokenType.IDENTIFIER,
+            "line": 6,
+            "col": 15,
+            "matched": "name"
+        },
+        {
+            "type": TokenType.RPARAN,
+            "line": 6,
+            "col": 19,
+            "matched": ")"
+        },
+        {
+            "type": TokenType.IDENTIFIER,
+            "line": 6,
+            "col": 21,
+            "matched": "typedef"
+        },
+        {
+            "type": TokenType.IDENTIFIER,
+            "line": 6,
+            "col": 29,
+            "matched": "struct"
+        },
+        {
+            "type": TokenType.IDENTIFIER,
+            "line": 6,
+            "col": 36,
+            "matched": "name"
+        },
+        {
+            "type": TokenType.TOKEN_CONCATINATION, 
+            "line": 6, 
+            "col": 40,
+            "matched": "##"
+        },
+        {
+            "type": TokenType.IDENTIFIER,
+            "line": 6,
+            "col": 42,
+            "matched": "_s"
+        },
+        {
+            "type": TokenType.IDENTIFIER,
+            "line": 6,
+            "col": 45,
+            "matched": "name"
+        },
+        {
+            "type": TokenType.TOKEN_CONCATINATION,
+            "line": 6,
+            "col": 49,
+            "matched": "##"
+        },
+        {
+            "type": TokenType.IDENTIFIER,
+            "line": 6,
+            "col": 51,
+            "matched": "_t"
+        }
     ]),
     ("Conditional statement", "#if X <= 5\n", 7, [
-        (TokenType.DIRECTIVE, 7, 1, "#if"),
-        (TokenType.IDENTIFIER, 7, 4, "X"),
-        (TokenType.LESS_THAN_OR_EQUAL, 7, 6, "<="),
-        (TokenType.INTEGER_CONST, 7, 9, "5")
+        {
+            "type": TokenType.DIRECTIVE,
+            "line": 7,
+            "col": 1,
+            "matched": "#if"
+        },
+        {
+            "type": TokenType.IDENTIFIER,
+            "line": 7,
+            "col": 4,
+            "matched": "X"
+        },
+        {
+            "type": TokenType.LESS_THAN_OR_EQUAL,
+            "line": 7,
+            "col": 6,
+            "matched": "<="
+        },
+        {
+            "type": TokenType.INTEGER_CONST,
+            "line": 7,
+            "col": 9,
+            "matched": "5"
+        }
     ]),
     ("Include statement", "#include <stdio.h>\n", 8, [
-        (TokenType.DIRECTIVE, 8, 1, "#include"),
-        (TokenType.FILENAME, 8, 9, "<stdio.h>")
+        {
+            "type": TokenType.DIRECTIVE,
+            "line": 8,
+            "col": 1,
+            "matched": "#include"
+        },
+        {
+            "type": TokenType.FILENAME,
+            "line": 8,
+            "col": 9,
+            "matched": "<stdio.h>"
+        }
     ]),
     ("Conditionals aren't files", "#pragma 34 <= TEST_1 || TEST_2 > 55\n", 9, [
-        (TokenType.DIRECTIVE, 9, 1, "#pragma"),
-        (TokenType.INTEGER_CONST, 9, 8, "34"),
-        (TokenType.LESS_THAN_OR_EQUAL, 9, 11, "<="),
-        (TokenType.IDENTIFIER, 9, 14, "TEST_1"),
-        (TokenType.OR, 9, 21, "||"),
-        (TokenType.IDENTIFIER, 9, 24, "TEST_2"),
-        (TokenType.GREATER_THAN, 9, 31, ">"),
-        (TokenType.INTEGER_CONST, 9, 33, "55")
+        {
+            "type": TokenType.DIRECTIVE,
+            "line": 9,
+            "col": 1,
+            "matched": "#pragma"
+        },
+        {
+            "type": TokenType.INTEGER_CONST,
+            "line": 9,
+            "col": 8,
+            "matched": "34"
+        },
+        {
+            "type": TokenType.LESS_THAN_OR_EQUAL,
+            "line": 9,
+            "col": 11,
+            "matched": "<="
+        },
+        {
+            "type": TokenType.IDENTIFIER,
+            "line": 9,
+            "col": 14,
+            "matched": "TEST_1"
+        },
+        {
+            "type": TokenType.OR,
+            "line": 9,
+            "col": 21,
+            "matched": "||"
+        },
+        {
+            "type": TokenType.IDENTIFIER,
+            "line": 9,
+            "col": 24,
+            "matched": "TEST_2"
+        },
+        {
+            "type": TokenType.GREATER_THAN,
+            "line": 9,
+            "col": 31,
+            "matched": ">"
+        },
+        {
+            "type": TokenType.INTEGER_CONST,
+            "line": 9,
+            "col": 33,
+            "matched": "55"
+        }
     ])
 ]
 
@@ -102,19 +287,55 @@ def test_tokenize_line(line_str, line_number, expected):
     assert_token_lists_equal(actual_list, expected)
 
 
+LINE_ENDINGS_TEST_DATA = [
+    {
+        "type": TokenType.DIRECTIVE,
+        "line": 0,
+        "col": 1,
+        "matched": "#define"
+    },
+    {
+        "type": TokenType.IDENTIFIER,
+        "line": 0,
+        "col": 8,
+        "matched": "TEST_LIST"
+    },
+    {
+        "type": TokenType.IDENTIFIER,
+        "line": 0,
+        "col": 18,
+        "matched": "a"
+    },
+    {
+        "type": TokenType.COMMA,
+        "line": 0,
+        "col": 19,
+        "matched": ","
+    },
+    {
+        "type": TokenType.IDENTIFIER,
+        "line": 1,
+        "col": 24,
+        "matched": "b"
+    },
+    {
+        "type": TokenType.COMMA,
+        "line": 1,
+        "col": 25,
+        "matched": ","
+    },
+    {
+        "type": TokenType.IDENTIFIER,
+        "line": 2,
+        "col": 30,
+        "matched": "c"
+    }
+]
+
+
 def test_tokenize_line_escaped_line_endings():
     cursor = StringCursor("#define TEST_LIST a, \\\n\tb, \\\n\tc\n")
     actual_list, line_offset = tokenize_line(cursor, 0)
 
-    expected_list = [
-        (TokenType.DIRECTIVE, 0, 1, "#define"),
-        (TokenType.IDENTIFIER, 0, 8, "TEST_LIST"),
-        (TokenType.IDENTIFIER, 0, 18, "a"),
-        (TokenType.COMMA, 0, 19, ","),
-        (TokenType.IDENTIFIER, 1, 24, "b"),
-        (TokenType.COMMA, 1, 25, ","),
-        (TokenType.IDENTIFIER, 2, 30, "c")
-    ]
-
-    assert_token_lists_equal(actual_list, expected_list)
+    assert_token_lists_equal(actual_list, LINE_ENDINGS_TEST_DATA)
     assert line_offset == 3
