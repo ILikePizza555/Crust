411e4750c394b04e7f7fb4384d1878fcd5722472
Izzy <avrisaac555@gmail.com>
2019-09-29 15:31:30 -0700

    Fixed tokenizer test to use new api with match object

--- test/test_preprocessor_tokenizer.py
+++ test/test_preprocessor_tokenizer.py
@@ -3,11 +3,19 @@ from src.preprocessor.tokenizer import tokenize_line, Token, TokenType, _tokeniz
 from src.useful import StringCursor
 
 
+def assert_token_equals(token: Token, type: TokenType, line: int, col: int, matched: str):
+    """Helper function to test token equality"""
+    assert token.token_type is type
+    assert token.line == line
+    assert token.col == col
+    assert token.match.group() == matched
+
+
 def test_tokenize_directive_valid_input():
     cursor = StringCursor("#include <iostream>")
     actual = _tokenize_directive(cursor, 0)
 
-    assert actual == Token(TokenType.DIRECTIVE, 0, 1, "include")
+    assert_token_equals(actual, TokenType.DIRECTIVE, 0, 1, "#include")
     assert cursor.position == 8
 
 
@@ -15,7 +23,7 @@ def test_tokenize_directive_null():
     cursor = StringCursor("# the rest of this should not be read")
     actual = _tokenize_directive(cursor, 0)
 
-    assert actual == Token(TokenType.DIRECTIVE, 0, 1, "")
+    assert_token_equals(actual, TokenType.DIRECTIVE, 0, 1, "#")
     assert cursor.position == 1
 
 
@@ -26,52 +34,52 @@ def test_tokenize_directive_error():
 
 
 test_data = [
-    ("Null directive", "#\n", 0, [Token(TokenType.DIRECTIVE, 0, 1, "")]),
-    ("No parameter directive", "#pragma\n", 0, [Token(TokenType.DIRECTIVE, 0, 1, "pragma")]),
+    ("Null directive", "#\n", 0, [(TokenType.DIRECTIVE, 0, 1, "")]),
+    ("No parameter directive", "#pragma\n", 0, [(TokenType.DIRECTIVE, 0, 1, "pragma")]),
     ("Integer constant", "#define TEST_INT 1234\n", 4, [
-        Token(TokenType.DIRECTIVE, 4, 1, "define"),
-        Token(TokenType.IDENTIFIER, 4, 8, "TEST_INT"),
-        Token(TokenType.INTEGER_CONST, 4, 17, "1234")
+        (TokenType.DIRECTIVE, 4, 1, "#define"),
+        (TokenType.IDENTIFIER, 4, 8, "TEST_INT"),
+        (TokenType.INTEGER_CONST, 4, 17, "1234")
     ]),
     ("Character constant", "#define TEST_CHAR 's'\n", 5, [
-        Token(TokenType.DIRECTIVE, 5, 1, "define"),
-        Token(TokenType.IDENTIFIER, 5, 8, "TEST_CHAR"),
-        Token(TokenType.CHAR_CONST, 5, 18, "'s'")
+        (TokenType.DIRECTIVE, 5, 1, "#define"),
+        (TokenType.IDENTIFIER, 5, 8, "TEST_CHAR"),
+        (TokenType.CHAR_CONST, 5, 18, "'s'")
     ]),
     ("Function macro", "#define FMACRO(name) typedef struct name##_s name##_t\n", 6, [
-        Token(TokenType.DIRECTIVE, 6, 1, "define"),
-        Token(TokenType.IDENTIFIER, 6, 8, "FMACRO"),
-        Token(TokenType.LPARAN, 6, 14, "("),
-        Token(TokenType.IDENTIFIER, 6, 15, "name"),
-        Token(TokenType.RPARAN, 6, 19, ")"),
-        Token(TokenType.IDENTIFIER, 6, 21, "typedef"),
-        Token(TokenType.IDENTIFIER, 6, 29, "struct"),
-        Token(TokenType.IDENTIFIER, 6, 36, "name"),
-        Token(TokenType.TOKEN_CONCATINATION, 6, 40, "##"),
-        Token(TokenType.IDENTIFIER, 6, 42, "_s"),
-        Token(TokenType.IDENTIFIER, 6, 45, "name"),
-        Token(TokenType.TOKEN_CONCATINATION, 6, 49, "##"),
-        Token(TokenType.IDENTIFIER, 6, 51, "_t")
+        (TokenType.DIRECTIVE, 6, 1, "#define"),
+        (TokenType.IDENTIFIER, 6, 8, "FMACRO"),
+        (TokenType.LPARAN, 6, 14, "("),
+        (TokenType.IDENTIFIER, 6, 15, "name"),
+        (TokenType.RPARAN, 6, 19, ")"),
+        (TokenType.IDENTIFIER, 6, 21, "typedef"),
+        (TokenType.IDENTIFIER, 6, 29, "struct"),
+        (TokenType.IDENTIFIER, 6, 36, "name"),
+        (TokenType.TOKEN_CONCATINATION, 6, 40, "##"),
+        (TokenType.IDENTIFIER, 6, 42, "_s"),
+        (TokenType.IDENTIFIER, 6, 45, "name"),
+        (TokenType.TOKEN_CONCATINATION, 6, 49, "##"),
+        (TokenType.IDENTIFIER, 6, 51, "_t")
     ]),
     ("Conditional statement", "#if X <= 5\n", 7, [
-        Token(TokenType.DIRECTIVE, 7, 1, "if"),
-        Token(TokenType.IDENTIFIER, 7, 4, "X"),
-        Token(TokenType.LESS_THAN_OR_EQUAL, 7, 6, "<="),
-        Token(TokenType.INTEGER_CONST, 7, 9, "5")
+        (TokenType.DIRECTIVE, 7, 1, "#if"),
+        (TokenType.IDENTIFIER, 7, 4, "X"),
+        (TokenType.LESS_THAN_OR_EQUAL, 7, 6, "<="),
+        (TokenType.INTEGER_CONST, 7, 9, "5")
     ]),
     ("Include statement", "#include <stdio.h>\n", 8, [
-        Token(TokenType.DIRECTIVE, 8, 1, "include"),
-        Token(TokenType.FILENAME, 8, 9, "<stdio.h>")
+        (TokenType.DIRECTIVE, 8, 1, "#include"),
+        (TokenType.FILENAME, 8, 9, "<stdio.h>")
     ]),
     ("Conditionals aren't files", "#pragma 34 <= TEST_1 || TEST_2 > 55\n", 9, [
-        Token(TokenType.DIRECTIVE, 9, 1, "pragma"),
-        Token(TokenType.INTEGER_CONST, 9, 8, "34"),
-        Token(TokenType.LESS_THAN_OR_EQUAL, 9, 11, "<="),
-        Token(TokenType.IDENTIFIER, 9, 14, "TEST_1"),
-        Token(TokenType.OR, 9, 21, "||"),
-        Token(TokenType.IDENTIFIER, 9, 24, "TEST_2"),
-        Token(TokenType.GREATER_THAN, 9, 31, ">"),
-        Token(TokenType.INTEGER_CONST, 9, 33, "55")
+        (TokenType.DIRECTIVE, 9, 1, "#pragma"),
+        (TokenType.INTEGER_CONST, 9, 8, "34"),
+        (TokenType.LESS_THAN_OR_EQUAL, 9, 11, "<="),
+        (TokenType.IDENTIFIER, 9, 14, "TEST_1"),
+        (TokenType.OR, 9, 21, "||"),
+        (TokenType.IDENTIFIER, 9, 24, "TEST_2"),
+        (TokenType.GREATER_THAN, 9, 31, ">"),
+        (TokenType.INTEGER_CONST, 9, 33, "55")
     ])
 ]
 
@@ -83,8 +91,11 @@ test_data = [
 def test_tokenize_line(line_str, line_number, expected):
     cursor = StringCursor(line_str)
 
-    actual, line_offset = tokenize_line(cursor, line_number)
-    assert actual == expected
+    actual_list, line_offset = tokenize_line(cursor, line_number)
+
+    assert len(actual_list) == len(expected)
+    for a, e in zip(actual_list, expected):
+        assert_token_equals(a, *e)
 
 
 def test_tokenize_line_escaped_line_endings():
