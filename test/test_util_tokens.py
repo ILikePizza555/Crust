448d86a92e982206b36b16ec6fe1c20f85f12700
Izzy <avrisaac555@gmail.com>
2019-10-01 20:50:48 -0700

    Updated documentation

--- test/test_util_tokens.py
+++ test/test_util_tokens.py
@@ -4,7 +4,10 @@ from .util_tokens import MockToken
 
 
 def test_token_equality():
-    """This test asserts that MockToken's equality operator works correctly."""
+    """
+    This test asserts that MockToken's equality operator works correctly.
+    This is mainly a sanity check.
+    """
     tok = Token(TokenType.DIRECTIVE, 3, 4, re.match(r"#(\S*)", "#test"))
     m_tok = MockToken(TokenType.DIRECTIVE, "#test", 3, 4)
     om_tok = MockToken(TokenType.IDENTIFIER, "NOTEQUAL", 6, 9)
