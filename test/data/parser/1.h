ab6c4e33ec22ba1714c5c619c50443294f737ec6
Izzy <avrisaac555@gmail.com>
2019-10-06 16:49:59 -0700

    Added some test data for parser test

---
+++ test/data/parser/1.h
@@ -0,0 +1,6 @@
+#include <begintest>
+#include "non-existant.h"
+
+#define FOOBAR "nice"
+
+int this_should_not_be_evaluated();
---
+++ test/data/parser/2.h
@@ -0,0 +1,17 @@
+#include <begintest>
+#if defined _WIN32
+    #include <win32.h>
+    #if FOOBAR == 32
+        #define TRANSGIRLS ARE GOOD
+    #endif
+#elif defined LINUX
+    #include <linux.h>
+    #if FOOBAR == 16
+        #define TRANSBOYS ARE GOOD
+    #endif
+#elif defined MACOS
+    #include <apple.h>
+    #if FOOBAR == 8
+        #define EWW CIS
+    #endif
+#endif
