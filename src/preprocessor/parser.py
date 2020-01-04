5e4a32f32ec1b10f032825ebf19006312d8a09cc
Izzy Lancaster <avrisaac555@gmail.com>
2020-01-04 15:45:25 -0800

    Added documentation to parser

--- src/preprocessor/parser.py
+++ src/preprocessor/parser.py
@@ -1,23 +1,15 @@
-from typing import Union, Iterable, Dict, List, Optional, Set, Iterator
+"""
+Defines objects and functions that act on tokens.
+
+Note that the primary purpose of the parser to is *organize* tokens, not evaluate them.
+"""
+from typing import Union, Iterable, Dict, List, Set, Iterator
 from itertools import takewhile
 from .tokenizer import TokenType, Token
 
 
 Literal = Union[str, int, float]
 IdentifierTable = Dict[str, Literal]
-
-
-def parse_literal(literal_token: Token) -> Literal:
-    """Parses a token of type num into a python object"""
-    if literal_token.type is TokenType.STRING_LITERAL:
-        return literal_token.match.group(0)
-    elif literal_token.type is TokenType.NUM_LITERAL:
-        try:
-            return int(literal_token.match.group(0))
-        except ValueError:
-            return float(literal_token.match.group(0))
-    else:
-        raise ValueError("primitive_token.type not set to a literal")
 
 
 def parse_identifier_list(tokens: Iterable[Token]) -> Iterable[Token]:
